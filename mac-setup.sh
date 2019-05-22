6ad2f111b4c8979acaaf1bd0eb071778831fdbd2
Tushar Tambe <44019295+tushartambe@users.noreply.github.com>
2019-05-22 16:58:31 +0530

    Initial commit. All steps are listed with commands

---
+++ mac-setup.sh
@@ -0,0 +1,131 @@
+#first install chrome and download iterm2 then follow these steps
+# Clone the .zshrc and .vimrc files here 
+        git clone https://github.com/tushartambe/dotfiles.git
+
+#homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+#zsh install
+brew install zsh
+
+#install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+#set zsh default shell
+chsh -s /bin/zsh
+
+#launch zsh afetr above steps
+
+#--------------------
+
+#zsh auto Suggestions
+brew install zsh-autosuggestions
+#you can do this mannually by putting this line into ~/.zshrc
+# 'source /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh'
+echo "source /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc 
+        #no need if you cloned .zshrc and .vimrc
+
+#Start a new terminal session.
+
+#--------------------
+
+#zsh syntax highlighting
+brew install zsh-syntax-highlighting
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
+        #no need if you cloned .zshrc and .vimrc
+
+#start new terminal session
+
+#--------------------
+#install powerlevel9K theme
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+
+#in .zshrc
+ZSH_THEME="powerlevel9k/powerlevel9k"
+        #no need if you cloned .zshrc and .vimrc
+
+#install and apply meslo LG M for powerline font
+#download color schemes for iterm2 [optional]
+
+#--------------------
+#install vim plugins
+
+mkdir -p ~/.vim/pack/myplugins/start
+
+#clone all these repos into ~/.vim/pack/myplugins/start folder
+cd ~/.vim/pack/myplugins/start
+
+git clone https://github.com/jiangmiao/auto-pairs.git
+git clone https://github.com/vim-scripts/AutoComplPop.git
+git clone https://github.com/scrooloose/nerdtree.git
+git clone https://github.com/yggdroot/indentline.git
+git clone https://github.com/itchyny/lightline.vim.git
+git clone https://github.com/flazz/vim-colorschemes.git
+git clone https://github.com/airblade/vim-gitgutter.git
+git clone https://github.com/pangloss/vim-javascript.git
+git clone https://github.com/terryma/vim-multiple-cursors.git
+git clone https://github.com/prettier/vim-prettier
+
+#--------------------
+#zsh tools
+brew install tree
+brew install ack
+brew install tig
+brew install bat
+brew install autojump
+
+#--------------------
+
+#install node 
+brew install node
+
+#--------------------
+#installing heroku 
+
+brew tap heroku/brew
+brew install heroku
+
+#--------------------
+#node tools
+
+npm install -g nyc
+npm install -g readline-sync
+npm install -g mocha
+npm install -g prettier
+npm install -g chalk
+
+#--------------------
+
+#VSCODE EXTENSIONS
+    #advanced-new-file
+    #Angular 2 TypeScript emmet
+    #Auto rename tag
+    #Better TOML
+    #Bracket pair colorizer
+    #change-case
+    #Docker
+    #mocha test explorer
+    #Org mode
+    #Prettier Code formatter
+    #Test explorer UI
+    #vscode-icons
+
+#--------------------
+
+#MAC APPLICATIONS
+   #Chrome
+   #elmedia player
+   #zoom client meeting
+   #android file transfer
+   #xmind
+   #slack
+   #evernote 
+   #firefox developer edition
+   #virtualbox
+   #openshot video editor
+   #docker
+   #intellij
+   #Android studio
+
+#--------------------
