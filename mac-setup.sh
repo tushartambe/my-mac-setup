2efb29e94fbc72a38d4a5273b7c91288d00dae84
Tushar Tambe <44019295+tushartambe@users.noreply.github.com>
2019-10-14 13:16:39 +0530

    Add powerlevel10k installation

--- mac-setup.sh
+++ mac-setup.sh
@@ -44,7 +44,7 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
 #--------------------
 #install powerlevel9K theme
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
-
+git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
 #in .zshrc
 ZSH_THEME="powerlevel9k/powerlevel9k"
         #no need if you cloned .zshrc and .vimrc
